c77327f40ff3e87e94cf77b81f0502bc1b6c9a85
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-05-24 13:11:52 +0200

    feat: create App component with input field and buttons for adding and deleting tasks

--- ToDO/src/App.jsx
+++ ToDO/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const App = () => {
+ return(
+  <main className="main-todo">
+    <section className="section-todo">
+      <div className="container-add-todo">
+      <input type="text" value="" className="input-task-todo"/>
+      <button type="button">Añadir</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="conatiner-list-todo">
+      <button type="button">Eliminar completados</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  </main>
+ )
+  
 }
 
 export default App
