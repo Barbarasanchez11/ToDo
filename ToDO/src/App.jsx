46e56c9120fabe1b7b242c150c5fd1077f931951
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-05-24 16:25:19 +0200

    Prevent duplicate tasks by checking existence before adding

--- ToDO/src/App.jsx
+++ ToDO/src/App.jsx
@@ -8,20 +8,33 @@ const App = () => {
    
   
     
-    const handleAddTask = () => {
-    setTasks([...tasks, newTask])
+  const handleAddTask = () => {
+   
+    const existTask = tasks.indexOf(newTask)     
+    if(existTask === -1 )setTasks([...tasks, newTask])  
     setNewTask('')
     
     }
  
-
-
   return(
   <main className="main-todo">
     <section className="section-todo">
+      <div>
+       <div>
+        <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)} className="input-task-todo"/>
+        <button type="button" onClick={handleAddTask}>Añadir</button>
+       </div>
+      </div>
       <div className="container-add-todo">
-      <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)} className="input-task-todo"/>
-      <button type="button" onClick={handleAddTask}>Añadir</button>
+
+      {tasks.map(task => {
+       return(
+       <>
+        <input type='checkbox'/>
+        <div key={task}>{task}</div>
+       </>
+      )}
+      )}
       </div>
       <div className="conatiner-list-todo">
       <button type="button">Eliminar completados</button>
